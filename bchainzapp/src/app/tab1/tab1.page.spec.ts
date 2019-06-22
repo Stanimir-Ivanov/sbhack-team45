ee6e8fb4a2e88181083a87703d3a2874b32a3918
Stefan Zurfluh <stefan.zurfluh@sygnum.com>
2019-06-22 14:18:49 +0200

    introducing tabs

--- bchainzapp/src/app/tab1/tab1.page.spec.ts
+++ bchainzapp/src/app/tab1/tab1.page.spec.ts
@@ -1,22 +1,21 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TopupPage } from './topup.page';
+import { Tab1Page } from './tab1.page';
 
-describe('TopupPage', () => {
-  let component: TopupPage;
-  let fixture: ComponentFixture<TopupPage>;
+describe('Tab1Page', () => {
+  let component: Tab1Page;
+  let fixture: ComponentFixture<Tab1Page>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TopupPage ],
+      declarations: [Tab1Page],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TopupPage);
+    fixture = TestBed.createComponent(Tab1Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
